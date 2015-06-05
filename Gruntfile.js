ab6da0f6b08de27054b20106f29fd9486e2dbee1
Edmund Quintero <edmundquintero@gmail.com>
2015-06-04 22:41:26 -0400

    All the fancy init app stuff. Moved /days into app/ directory for linting.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    
+    jshint: {
+      options: grunt.file.readJSON('.jshintrc'),
+      src: [ 'app/**/*.js' ]
+    },
+
+    watch: {
+      scripts: {
+        files: ['app/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: '',
+          open: 'http://127.0.0.1:9000/app/index.html',
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask( 'serve', ['jshint', 'connect', 'watch']);
+ 
+  grunt.registerTask( 'default', [
+    'jshint'
+  ]);
+
+};
